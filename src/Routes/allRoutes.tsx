a2c16a64fc4d1ce272f1f387507284dbfbc340ba
AbdelrahmanAlsadder <aboodalsadder@gmail.com>
2024-11-04 16:47:34 +0300

    Up
    Cleaned the code after editing the nav bar

--- src/Routes/allRoutes.tsx
+++ src/Routes/allRoutes.tsx
@@ -70,17 +70,9 @@ import PolarChart from "../pages/Components/Apexcharts/PolarArea/Index";
 import AddInvoice from "../pages/InvoiceManagement/AddInvoice/index";
 import InvoiceDetails from "../pages/InvoiceManagement/InvoiceDetails/index";
 import AddProduct from "../pages/InvoiceManagement/AddProduct";
-import NewTransaction from "../pages/InvoiceManagement/NewTransaction/index";
-import TransactionList from "../pages/InvoiceManagement/TransactionList/index";
-import Taxes from "../pages/InvoiceManagement/Taxes/index";
-import SalesReport from "../pages/InvoiceManagement/Report/SalesReport/index";
-import ExpensesReport from "../pages/InvoiceManagement/Report/ExpensesReport/index";
 import Invoice from "../pages/InvoiceManagement/Invoice/index";
-import PaymentSummary from "../pages/InvoiceManagement/Report/PaymentSummary";
-import Users from "../pages/InvoiceManagement/Users";
 import ProductList from "../pages/InvoiceManagement/ProductList";
 import Payments from "../pages/InvoiceManagement/Payments";
-
 import Login from "../pages/Authentication/Login";
 import Logout from "../pages/Authentication/Logout";
 import ForgotPassword from "../pages/Authentication/ForgotPassword";
@@ -107,15 +99,8 @@ const authProtectedRoutes: Array<RouteObject> = [
 
   { path: "/invoice", component: <Invoice /> },
   { path: "/invoice-add", component: <AddInvoice /> },
-  { path: "/payment-summary", component: <PaymentSummary /> },
   { path: "/invoice-details", component: <InvoiceDetails /> },
   { path: "/product-add", component: <AddProduct /> },
-  { path: "/transaction-new", component: <NewTransaction /> },
-  { path: "/transaction-list", component: <TransactionList /> },
-  { path: "/taxes", component: <Taxes /> },
-  { path: "/sale-report", component: <SalesReport /> },
-  { path: "/expenses-report", component: <ExpensesReport /> },
-  { path: "/user", component: <Users /> },
   { path: "/product-list", component: <ProductList /> },
   { path: "/payments", component: <Payments /> },
 
